2e90d9a1fa35cc59b0c9a29e5f43ab60c82b1b73
Lisa Falko <liza199797@mail.ru>
2017-03-10 21:29:17 +0300

    html minification added

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
-var gulp = require('gulp'),
-	concat = require('gulp-concat'),
+var gulp = require('gulp');
+	
+var concat = require('gulp-concat'),
 	autoprefixer = require('gulp-autoprefixer'),
 	cleanCSS = require('gulp-clean-css');
 
-gulp.task("default", function () {
-	gulp.src("src/css/*.css")
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('default', function () {
+	gulp.src('src/css/*.css')
 		.pipe(concat('bundle.css'))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions']
 		}))
 		.pipe(cleanCSS())
-		.pipe(gulp.dest("src/css/"))
+		.pipe(gulp.dest('src/css/'))
+});
+
+gulp.task('html', function () {
+	gulp.src('src/index.html')
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
+		.pipe(gulp.dest('./'));
 });
