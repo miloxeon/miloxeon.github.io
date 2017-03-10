cb62ebf25d1b8547545ecb4f1de5e93a5241b8a1
uyouthe <melvoloskov@ya.ru>
2017-03-10 21:41:26 +0300

    uglify added, gulpfile finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-var gulp = require('gulp');
-	
-var concat = require('gulp-concat'),
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
 	autoprefixer = require('gulp-autoprefixer'),
-	cleanCSS = require('gulp-clean-css');
+	cleanCSS = require('gulp-clean-css'),
+	uglify = require('gulp-uglify'),
+	htmlmin = require('gulp-htmlmin'),
+	inlineSource = require('gulp-inline-source');
 
-var htmlmin = require('gulp-htmlmin');
 
-gulp.task('default', function () {
+gulp.task('css', function () {
 	gulp.src('src/css/*.css')
 		.pipe(concat('bundle.css'))
 		.pipe(autoprefixer({
@@ -16,10 +17,21 @@ gulp.task('default', function () {
 		.pipe(gulp.dest('src/css/'))
 });
 
+gulp.task('js', function () {
+	gulp.src('src/js/*.js')
+		.pipe(concat('bundle.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('src/js/'))
+});
+
 gulp.task('html', function () {
 	gulp.src('src/index.html')
 		.pipe(htmlmin({
-			collapseWhitespace: true
+			collapseWhitespace: true,
+			removeComments: true
 		}))
+		.pipe(inlineSource())
 		.pipe(gulp.dest('./'));
 });
+
+gulp.task('default', ['css', 'js', 'html']);
