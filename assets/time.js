3a928434061e40cf3acdf42ffa0b27c79406eb43
uyouthe <melvoloskov@ya.ru>
2018-11-26 00:22:18 +0300

    clock fixed

--- assets/time.js
+++ assets/time.js
@@ -1,15 +1,15 @@
 'use strict'
 
-Date.prototype.today = function () { 
+Date.prototype.today = function () {
     return (this.getDate() < 10 ? '0' : '') + this.getDate() + '.' +
-           (this.getMonth() + 1 < 10 ? '0' : '') + (this.getMonth() + 1) + '.' + 
+           (this.getMonth() + 1 < 10 ? '0' : '') + (this.getMonth() + 1) + '.' +
             this.getFullYear()
 }
 
 Date.prototype.timeNow = function () {
-    return this.getHours() < 10 ? '0' : '' + this.getHours() + ':' +
-          (this.getMinutes() < 10 ? '0' : '') + this.getMinutes() + ':' +
-          (this.getSeconds() < 10 ? '0' : '') + this.getSeconds()
+    return (this.getHours() < 10 ? '0' : '') + this.getHours() + ':' +
+           (this.getMinutes() < 10 ? '0' : '') + this.getMinutes() + ':' +
+           (this.getSeconds() < 10 ? '0' : '') + this.getSeconds()
 }
 
 var output = document.getElementById('time')
@@ -17,6 +17,7 @@ var output = document.getElementById('time')
 function update () {
     var date = new Date()
     var clock = date.today() + ', ' + date.timeNow()
+
     if (/\d\d.\d\d.\d\d\d\d, \d\d:\d\d:\d\d/gm.test(clock)) {
     	output.innerHTML = clock
     }
