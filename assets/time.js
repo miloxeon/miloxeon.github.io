ece9b33411f86fa067930e47792979b291b57432
uyouthe <melvoloskov@ya.ru>
2020-09-09 19:06:19 +0300

    job help, new style, new code

--- assets/time.js
+++ assets/time.js
@@ -1,24 +1,15 @@
 'use strict'
 
-Date.prototype.today = function () {
-    return (this.getDate() < 10 ? '0' : '') + this.getDate() + '.' +
-           (this.getMonth() + 1 < 10 ? '0' : '') + (this.getMonth() + 1) + '.' +
-            this.getFullYear()
-}
+const output = document.getElementById('time')
 
-Date.prototype.timeNow = function () {
-    return (this.getHours() < 10 ? '0' : '') + this.getHours() + ':' +
-           (this.getMinutes() < 10 ? '0' : '') + this.getMinutes() + ':' +
-           (this.getSeconds() < 10 ? '0' : '') + this.getSeconds()
-}
+const update = () => {
+    const dateString = new Date().toISOString()
+    const date = dateString.slice(0, 10)
+    const time = dateString.slice(11, 19)
 
-var output = document.getElementById('time')
+    const clock = `${date}, ${time}`
 
-function update () {
-    var date = new Date()
-    var clock = date.today() + ', ' + date.timeNow()
-
-    if (/\d\d.\d\d.\d\d\d\d, \d\d:\d\d:\d\d/gm.test(clock)) {
+    if (/\d\d\d\d\-\d\d\-\d\d, \d\d:\d\d:\d\d/gm.test(clock)) {
     	output.innerHTML = clock
     }
 }
