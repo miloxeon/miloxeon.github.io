63d0cebe44b806f64373d48152e6c7ef23e6fbe2
mvoloskov <hello@miloslav.website>
2020-11-20 23:38:27 +0300

    prettier, filters removed

--- assets/time.js
+++ assets/time.js
@@ -3,15 +3,14 @@
 const output = document.getElementById('time')
 
 const update = () => {
-    const dateString = new Date().toISOString()
-    const date = dateString.slice(0, 10)
-    const time = dateString.slice(11, 19)
+	const dateString = new Date().toISOString()
+	const date = dateString.slice(0, 10)
+	const time = dateString.slice(11, 19)
+	const clock = `${date}, ${time}`
 
-    const clock = `${date}, ${time}`
-
-    if (/\d\d\d\d\-\d\d\-\d\d, \d\d:\d\d:\d\d/gm.test(clock)) {
-    	output.innerHTML = clock
-    }
+	if (/\d\d\d\d\-\d\d\-\d\d, \d\d:\d\d:\d\d/gm.test(clock)) {
+		output.innerHTML = clock
+	}
 }
 
 setInterval(update, 1000)
